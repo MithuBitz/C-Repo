8e7ebb8a75699fdf8309a2851c8418b28d1a14bc
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2024-09-10 23:29:03 +0530

    Variable and Const in C++

--- 01_Besic/hello.cpp
+++ 01_Besic/hello.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-// using namespace std;
+using namespace std;
 //load specific method from namespace
-using std::cout;
+// using std::cout;
 
 // Createing custom namespace
 // namespace myName{
@@ -12,10 +12,28 @@ using std::cout;
 // }
 
 int main(){
-    cout << "Hello World from Mibits! Do you know me";
+    cout << "Hello World from Mibits! Do you know me" << endl ;
     //If we dont want to load all std namespace, load only required then
     // std::cout << "Hello World from Mibits! Do you know me";
     
     //We can use our own namespace
     // myName::display();
+
+    //Declare a constant which cannot be changed
+    const int gameLevel = 5;
+    cout << "Total Game level is : " << endl;
+    cout << gameLevel << endl;
+
+    //Declare a variable and initialize it in two lines
+    int score;
+    score = 100;
+    cout << "Your score is : " << endl;
+    cout << score << endl;
+
+    //Declare a variable and initialize it in one line
+    int topScore = 500;
+    cout << "Game top score is : " << endl;
+    cout << topScore;
+
+    return 0;
 }
--- README.md
+++ README.md
@@ -18,5 +18,9 @@
 
 - Namespace in c++ is like a region where we can decide what method should be run.
 - There are many namespace available like std. Some of then are: Qt framework, Eigen, gtest, etc.
-- We can load whole namespace like : ```using namespace std;``` Here namespace is std
-- Load a specific method from a namespace : ``` using std::cout; ```
+- We can load whole namespace like : `using namespace std;` Here namespace is std
+- Load a specific method from a namespace : `using std::cout;`
+
+## 4. Data manipualtion in C++ :
+
+- In any programming language there are three step to oparate. These are like `Store` data, `Process` data and also `Display` the data to the user. In Store we can study about datatypes and constant with help of which we can store the data. In Process we can study about loops, oop, functions, etc. And in Display we can study about how the data will be display to the user.
